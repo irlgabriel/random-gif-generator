d9a2a90b295c9c7b135825fbb5cee7e69119be93
irlgabriel <radu.gabriel131@gmail.com>
2020-10-09 20:25:24 +0300

    added flash messages

--- src/script.js
+++ src/script.js
@@ -11,24 +11,22 @@ form.addEventListener('submit', (e) => {
 })
 
 async function getGif(query) {
-  const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=OD3jwQFDqhiyVNJDY14lvnQDmYngMfqG&s=${query}`, {mode: 'cors'})
-  response.json().then(function(response) {
+  try {
+    const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=OD3jwQFDqhiyVNJDY14lvnQDmYngMfqG&s=${query}`, {mode: 'cors'})
+    const gifData = await response.json();
     const img = document.createElement('img');
     img.classList.add("image")
-    img.src = response.data.images.original.url;
-
+    img.src = gifData.data.images.original.url;
     images.appendChild(img)
-  })
-  .catch((err) => {
+
+    } catch(err) {
     const p = document.createElement('p');
     p.innerHTML = err.message;
     p.classList.add('flash');
-    p.style.position = "relative";
-    
     container.appendChild(p);
 
     setTimeout(() => {
       p.remove();
     }, 3000)
-  })
+  } 
 }
